cab919642a33d005ff1142d301e9c71dfd59013c
Bojan Ivkovic <ivkovic.bojan@gmail.com>
2017-05-22 23:13:27 +0200

    Dodati komentari u deltoidu

--- deltoida.m
+++ deltoida.m
@@ -8,10 +8,10 @@ clc
 
 %%
 
-% t = 0:.1:3*pi/2;
-alfa = 0:.1:10*pi;
+% alfa = 0:.1:3*pi/2; % otvorena
+alfa = 0:.1:10*pi; % zatvorena
 a = 80; % poluprecnik
-k = 3;
+k = 3; % broj "vrhova", sa 4 lici na astroidu
 x = a*(k-1)*cos(alfa)+a*cos((k-1)*alfa)+300;
 y = a*(k-1)*sin(alfa)-a*sin((k-1)*alfa)+300;
 figure
