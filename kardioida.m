5d38fd6d5a98108e32fcb5a0825f10385619b9db
Bojan Ivkovic <ivkovic.bojan@gmail.com>
2017-05-22 23:17:58 +0200

    Dodati komentari i naslov za kardioidu

--- kardioida.m
+++ kardioida.m
@@ -10,8 +10,9 @@ clc
 
 alfa = 0:0.01:100*pi;
 a = 150;
-y = a*(1+cos(alfa)).*cos(alfa)+200;
-x = a*(1+cos(alfa)).*sin(alfa)+250;
+y = a*(1+cos(alfa)).*cos(alfa)+200; % po y koordinati pomereno za 200
+x = a*(1+cos(alfa)).*sin(alfa)+250; % po x koordinati pomereno za 250
 
 figure
 plot(x,y)
+title('Kardioida')
